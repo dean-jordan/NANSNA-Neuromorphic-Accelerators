d75eae6cee45e029f4580663bdb773d5c530be72
next2523 <jordan.dean161@gmail.com>
2024-09-30 09:09:05 -0400

    Created initial positional encoding modules

--- NANSNA/Decoder/decoder_positional.py
+++ NANSNA/Decoder/decoder_positional.py
@@ -18,3 +18,21 @@ import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+
+import math
+
+class DecoderPositionalEncoding(nn.Module):
+    def __init__(self, model_dimensionality, max_sequence_length):
+        super(DecoderPositionalEncoding, self).__init__()
+
+        encoding_tensors = torch.zeros(max_sequence_length, model_dimensionality)
+        positonal_tensors = torch.arange(0, max_sequence_length, dtype=torch.float).unsqueeze(1)
+        divided_term = torch.exp(torch.arange(0, model_dimensionality, 2).float() * -(math.log(10000.0) / model_dimensionality))
+
+        encoding_tensors[:, 0::2] = torch.sin(positonal_tensors * divided_term)
+        encoding_tensors[:, 1::2] = torch.cos(positonal_tensors * divided_term)
+
+        self.register_buffer('encoding_tensors', encoding_tensors.unsqueeze(0))
+
+    def forward(self, x):
+        return x + self.encoding_tensors[:, :x.size(1)]
