5276d64d9f418429babf1ac8d0d0323e2723202d
next2523 <jordan.dean161@gmail.com>
2024-09-30 20:13:34 -0400

    Created initial encoder/decoder feedforward

--- NANSNA/Decoder/decoder_feedforward.py
+++ NANSNA/Decoder/decoder_feedforward.py
@@ -18,3 +18,74 @@ import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+
+from Layers import decoder_recurrent
+from Activation import relu
+
+class DecoderFeedForwardNetwork(nn.Module):
+    def __init__(self, model_dimensionality, feedforward_dimensionality):
+        super(DecoderFeedForwardNetwork, self).__init__()
+
+        self.activation = relu.ReluActivation()
+        
+        self.recurrent1 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent2 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent3 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent4 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent5 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent6 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent7 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent8 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent9 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent10 = decoder_recurrent.DecoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        
+    def forward(self, x):
+        return self.recurrent10(
+            self.activation(
+                self.recurrent9(
+                    self.activation(
+                        self.recurrent8(
+                            self.activation(
+                                self.recurrent7(
+                                    self.activation(
+                                        self.recurrent6(
+                                            self.activation(
+                                                self.recurrent5(
+                                                    self.activation(
+                                                        self.recurrent4(
+                                                            self.activation(
+                                                                self.recurrent3(
+                                                                    self.activation(
+                                                                        self.recurrent2(
+                                                                            self.activation(
+                                                                                self.recurrent1(
+                                                                                    self.activation(x)
+                                                                                )
+                                                                            )
+                                                                        )
+                                                                    )
+                                                                )
+                                                            )
+                                                        )
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
--- NANSNA/Encoder/encoder_feedforward.py
+++ NANSNA/Encoder/encoder_feedforward.py
@@ -20,10 +20,13 @@ import matplotlib.pyplot as plt
 import itertools
 
 from Layers import encoder_recurrent
+from Activation import relu
 
 class EncoderFeedForwardNetwork(nn.Module):
     def __init__(self, model_dimensionality, feedforward_dimensionality):
         super(EncoderFeedForwardNetwork, self).__init__()
+
+        self.activation = relu.ReluActivation()
         
         self.recurrent1 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
                                                                   feedforward_dimensionality)
@@ -31,3 +34,58 @@ class EncoderFeedForwardNetwork(nn.Module):
                                                                   feedforward_dimensionality)
         self.recurrent3 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
                                                                   feedforward_dimensionality)
+        self.recurrent4 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent5 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent6 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent7 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent8 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent9 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        self.recurrent10 = encoder_recurrent.EncoderRecurrentLayer(model_dimensionality, feedforward_dimensionality,
+                                                                  feedforward_dimensionality)
+        
+    def forward(self, x):
+        return self.recurrent10(
+            self.activation(
+                self.recurrent9(
+                    self.activation(
+                        self.recurrent8(
+                            self.activation(
+                                self.recurrent7(
+                                    self.activation(
+                                        self.recurrent6(
+                                            self.activation(
+                                                self.recurrent5(
+                                                    self.activation(
+                                                        self.recurrent4(
+                                                            self.activation(
+                                                                self.recurrent3(
+                                                                    self.activation(
+                                                                        self.recurrent2(
+                                                                            self.activation(
+                                                                                self.recurrent1(
+                                                                                    self.activation(x)
+                                                                                )
+                                                                            )
+                                                                        )
+                                                                    )
+                                                                )
+                                                            )
+                                                        )
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
